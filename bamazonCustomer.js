cf552d7612e330a55d5e9bfa0566ef08a60253e0
Robert Flanagan <robertflanagan95@gmail.com>
2018-12-21 00:30:02 -0500

    finished customer CLI

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,56 +19,78 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  afterConnection();
+  customerPrompt();
 });
 
-function afterConnection() {
-  connection.query("SELECT * FROM products", function(err, res) {
-    if (err) throw err;
-    for (var i = 0; i < res.length; i++) {
-        console.log("~~~~~~~~~~");
-        console.log("Item ID: " + res[i].item_id);
-        console.log("Product Name: " + res[i].product_name);
-        console.log("Department: " + res[i].department_name);
-        console.log("Price: $" + res[i].price);
-        console.log("~~~~~~~~~~");
-    }
-    customerPrompt();
-  });
-}
-
 function customerPrompt() {
-    inquirer.prompt([
-        {
-            name: "itemInput",
-            message: "Enter the Item ID of the item you would like to buy.",
-            type: "input"
-        },
-        {
-            name: "qtyInput",
-            message: "Enter the quantity you would like to buy.",
-            type: "input"
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log("~~~~~~~~~~");
+            console.log("Item ID: " + res[i].item_id);
+            console.log("Product Name: " + res[i].product_name);
+            console.log("Department: " + res[i].department_name);
+            console.log("Price: $" + res[i].price);
+            console.log("Qty: " + res[i].stock_quantity);
+            console.log("~~~~~~~~~~");
         }
-    ]).then(function(res) {
-        if (!res.itemInput || !res.qtyInput) {
-            console.log("Please enter all information.");
-            customerPrompt();
-        }
-        else if (isNaN(parseInt(res.itemInput))) {
-            console.log("The item number must be an integer.");
-            customerPrompt();
-        }
-        else if (isNaN(parseInt(res.qtyInput))) {
-            console.log("The quantity must be an integer.");
-            customerPrompt();
-        }
-        else {
-            console.log("Searching for items...");
-            stockSearch();
-        }
-    });
-}
 
-function stockSearch() {
-    connection.end();
+        inquirer.prompt([
+            {
+                name: "itemInput",
+                message: "Enter the Item ID of the item you would like to buy.",
+                type: "input"
+            },
+            {
+                name: "qtyInput",
+                message: "Enter the quantity you would like to buy.",
+                type: "input"
+            }
+        ]).then(function(response) {
+            if (!response.itemInput || !response.qtyInput) {
+                console.log("Please enter all information.");
+                customerPrompt();
+            }
+            else if (isNaN(parseInt(response.itemInput))) {
+                console.log("The item number must be an integer.");
+                customerPrompt();
+            }
+            else if (isNaN(parseInt(response.qtyInput))) {
+                console.log("The quantity must be an integer.");
+                customerPrompt();
+            }
+            else {
+                console.log("Searching for items...");
+                var chosenItem;
+                for (var i = 0; i < res.length; i++) {
+                    if (res[i].item_id === parseInt(response.itemInput)) {
+                        chosenItem = res[i];
+                    }
+                }
+
+                var newQuantity = chosenItem.stock_quantity - response.qtyInput;
+            
+                if (chosenItem.stock_quantity > parseInt(response.qtyInput)) {
+                    connection.query("UPDATE products SET ? WHERE ?",
+                    [
+                        {
+                            stock_quantity: newQuantity
+                        },
+                        {
+                            item_id: chosenItem.item_id
+                        }
+                    ], function(error) {
+                        if (error) throw error;
+                        console.log("Items successfully purchased.");
+                        var totalCost = (response.qtyInput) * (chosenItem.price);
+                        console.log("Total cost: $" + totalCost);
+                        connection.end();
+                    });
+                } else {
+                    console.log("Not enough in stock.");
+                    connection.end();
+                }
+            }
+        });
+    });
 }
